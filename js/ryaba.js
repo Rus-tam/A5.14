adb144d550cf48683940c2d9a2ab9f3f5470bbd6
Rus-tam <56486665+Rus-tam@users.noreply.github.com>
2020-02-02 22:13:54 +0500

    Add files via upload

---
+++ js/ryaba.js
@@ -0,0 +1,39 @@
+const dataURL = "https://api.myjson.com/bins/jcmhn";
+const variables = [
+	"var1",
+	"var2",
+	"var3",
+	"var4",
+	"var5",
+	"var6",
+	"speach"
+]
+
+function Values() {
+	let obj = {};
+	variables.forEach(function(field) {
+		obj[field] = $("input[name=" + field + "]")[0].value
+	});
+	return obj;
+}
+
+function handleButton(event) {
+	$.getJSON(dataURL, handleData);
+	event.preventDefault();
+}
+
+function handleData(data) {
+	let message = "";
+	let values = Values();
+	data["text"].forEach(function(line) {
+		for (key in values) {
+			line = line.replace("{" + key + "}", values[key]);
+		}
+		message = message + line + "<br>";
+	});
+    $("div#result").html(message);
+}
+function init() {
+	$("#button-fetch").click(handleButton);
+}
+$(document).ready(init);
